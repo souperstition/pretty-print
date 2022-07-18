48d769ef75c14604bf033565e5665cb78b59dd6a
Danielle Lyle <karendlyle@gmail.com>
2022-07-18 18:36:53 +0200

    refactor stylesheets and update README

--- README.md
+++ README.md
@@ -1,39 +1,58 @@
-# Pretty Print
+[![demo-gif](https://cdn.glitch.me/2a75c8f8-e3cd-4a74-9533-882132c11066/demo.gif?v=1658161279605)](https://cdn.glitch.me/2a75c8f8-e3cd-4a74-9533-882132c11066/demo.gif?v=1658161279605)
 
-## Overview
+# Overview
 ---
 
 Sometimes it's handy to just have all your cards and their descriptions in one list. Maybe you want to print out your cards or export them to PDF.
 
 With this power-up, you can choose which lists you want. The list, all its cards, their labels, and their descriptions will appear, fully formatted, ready to print or save.
 
-## How to Use
+# How to Use
 ---
 
-Try these tips to get the most out of this power-up.
+## Step 1: Click the "Pretty Print" board button: ![](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/115f6d5e-1351-47a5-a99e-425abe5d2ebc.image.png?v=1658161417410)
 
-### Tip #1:
+## Step 2: Adjust your preferences.
+
+[![](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/e1cd3bb1-af44-4842-b255-07e3042be929.image.png?v=1658160002272)](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/e1cd3bb1-af44-4842-b255-07e3042be929.image.png?v=1658160002272)
+
+## Step 3: Print or Export!
+
+Press the print button:
+[![](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/09c33d9a-7b9f-49c3-b1bc-1a173191f11b.image.png?v=1658160019449)](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/09c33d9a-7b9f-49c3-b1bc-1a173191f11b.image.png?v=1658160019449)
+
+Once you are satisfied with your settings, you can export your board to PDF...
+[![](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/ea3cf692-4799-4016-9967-51f0cdf0aae9.image.png?v=1658160033314)](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/ea3cf692-4799-4016-9967-51f0cdf0aae9.image.png?v=1658160033314)
+
+...or send it to the printer!
+[![](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/37b88f52-ca60-4a02-8ad0-a908d6fd88cb.image.png?v=1658160050454)](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/37b88f52-ca60-4a02-8ad0-a908d6fd88cb.image.png?v=1658160050454)
+
+# Tips:
+---
+
+## Tip #1:
 
 You can use this power-up to export your board into a nicely formatted PDF document. Instead of choosing a printer, select "Save as PDF" in your print settings:
 
-![save as pdf](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/save-pdf.png?v=1657900169598)
+[![save as pdf](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/save-pdf.png?v=1657900169598)](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/save-pdf.png?v=1657900169598)
 
 ---
 
-### Tip #2: 
+## Tip #2: 
 
-Make sure to check your printer settings if you aren't happy with how your page looks.
+The default margins are set to: 
 
-![printer settings](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/print-settings.png?v=1657899461068)
+- **Paper Size:** A4
+- **Top and Bottom Margins:** 0.25in
+- **Left and Right Margins:** 0
 
-I usually go with the following:
+If you wish to change this, you can do so in the print settings (in Chrome, check the "More Settings" option):
 
-- Top and Bottom Margins: 0.25in
-- Left and Right Margins: 0
+[![printer settings](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/print-settings.png?v=1657899461068)](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/print-settings.png?v=1657899461068)
 
 ---
 
-### Tip #3:
+## Tip #3:
 
 If you have a lot of empty page space happening on your print preview, here are some things you can try to get it the way you like:
 
@@ -42,10 +61,18 @@ If you have a lot of empty page space happening on your print preview, here are 
 
 ---
 
-### Tip #4:
+## Tip #4:
 
 You can disable the card colors to save printer ink! Just check the box that says "Card backgrounds" and that formatting will disappear.
 
 If you do want the colors to show up in print; however, make sure you double check your printer settings and turn on background graphics:
 
-![enable background graphics](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/background-graphics.png?v=1657900036578)
+[![enable background graphics](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/background-graphics.png?v=1657900036578)](https://cdn.glitch.global/2a75c8f8-e3cd-4a74-9533-882132c11066/background-graphics.png?v=1657900036578)
+
+# Help
+---
+
+If you need assistance, feel free to get in touch:
+
+- Email: [karendlyle@gmail.com](mailto:karendlyle@gmail.com)
+- Github: [souperstition](https://github.com/souperstition)
--- public/js/print.mjs
+++ public/js/print.mjs
@@ -27,29 +27,38 @@ const toggleClassName = (selector, element, className) => {
   })
 } 
 
-const styleSelector = document.getElementById('style-selector');
+// USER OPTION CHECKBOXES
+const allowStyles = document.getElementById('allow-styles');
 const breakLists = document.getElementById('break-lists');
 const breakCards = document.getElementById('break-cards');
 const filterImgs = document.getElementById('filter-images');
 const cardTitles = document.getElementById('card-titles');
 const cardLabels = document.getElementById('card-labels');
 const cardDescs = document.getElementById('card-descs');
+const lastActivity = document.getElementById('last-activity');
+const dueDates = document.getElementById('due-dates');
+const showMembers = document.getElementById('show-members');
+const showAttachments = document.getElementById('show-attachments');
 const listsOnly = document.getElementById('lists-only');
 
-toggleClassName(styleSelector, wrapper, 'styles');
+// FUNCTION CALL TO TOGGLE USER OPTIONS
+toggleClassName(allowStyles, wrapper, 'no-styles');
 toggleClassName(breakLists, mainSection, 'list-break');
 toggleClassName(breakCards, mainSection, 'card-break');
 toggleClassName(filterImgs, mainSection, 'img-filter');
 toggleClassName(cardTitles, mainSection, 'no-titles');
 toggleClassName(cardLabels, mainSection, 'no-labels');
 toggleClassName(cardDescs, mainSection, 'no-descs');
+toggleClassName(lastActivity, mainSection, 'no-last-active');
+toggleClassName(dueDates, mainSection, 'no-due-dates');
+toggleClassName(showMembers, mainSection, 'no-members');
+toggleClassName(showAttachments, mainSection, 'no-attachments');
 toggleClassName(listsOnly, mainSection, 'lists-only');
 
 t.render(() => { 
   return Promise.all([
     t.board('name'), // board name
-    t.lists('name', 'id'), // get list names and ids
-    t.cards('name', 'desc', 'labels', 'idList', 'cover') // get cards and desired info
+    t.lists('name', 'id', 'cards') // get lists
   ])
   .spread((board, lists, cards) => {
     title.innerText = board.name; // add board name to the top of the page
@@ -57,7 +66,7 @@ t.render(() => {
     // iterate through each list
     lists.forEach((list) => {
       // CHECKBOXES
-      // add the checkboxes to the top of the page
+      // create a checkbox for each list and add the checkboxes to the top of the page
       const listSelect = document.createElement('span');
       const listCheckBox = document.createElement('input');
       listCheckBox.setAttribute('type', 'checkbox');
@@ -72,7 +81,6 @@ t.render(() => {
       listSelect.appendChild(checkLabel);
       listBoxes.appendChild(listSelect);
       
-      
       // LIST CONTAINER
       const listSection = document.createElement('section');
       listSection.classList.add('list-section');
@@ -81,28 +89,27 @@ t.render(() => {
       mainSection.appendChild(listSection);
       listSection.innerHTML += `<h2 class="list-title">${list.name}</h2>`;
       
+      // call function to toggle list on/off
       const listDiv = document.getElementById(`${list.name}-title`);
       toggleClassName(listCheckBox, listDiv, 'print');
       
-      // FILTER OUT CARDS FROM THE CURRENT LIST
-      const filteredCards = cards.filter(card => {
-        return card.idList === list.id
-      });
       // CARD CONTAINER
-      filteredCards.forEach(async (card) => {
+      list.cards.forEach(async (card) => {
         const cardSection = document.createElement('section');
         cardSection.classList.add('card-section');
-
+        
+        // CARD BACKGROUND
+        // if the card cover has a background image, use it; otherwise give the color to its class list
         if (card.cover.idUploadedBackground !== null) {
-          cardSection.setAttribute('style', `background-image: url(${card.cover.sharedSourceUrl}; background-repeat: no-repeat; background-size: cover)`)
+          cardSection.setAttribute('style', `background-image: url(${card.cover.sharedSourceUrl}); background-size: cover; background-repeat: no-repeat;`)
         } else {
           cardSection.classList.add(`${card.cover.color}-card`);
         }
         
         cardSection.innerHTML += `<h2 class="card-title">${card.name}</h2>`;
 
-  
-        // IF THE CARD HAS LABELS, ADD THEM HERE
+        // CARD LABELS
+        // if the card has labels, add them here
         if(card.labels.length) {
           const labels = document.createElement('p');
           labels.classList.add('labels');
@@ -112,17 +119,83 @@ t.render(() => {
           cardSection.appendChild(labels);
         }
         
-        // CONVERT MARKDOWN TO HTML
+        // DATES SECTION
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const showDates = document.createElement('div');
+        showDates.classList.add('dates-section');
+        
+        // LAST ACTIVITY
+        // card.dateLastActivity
+        const lastActive = document.createElement('span');
+        lastActive.classList.add('last-active');
+        const activeDate = new Date(card.dateLastActivity);
+        lastActive.innerHTML += `<i title="Last Activity" class="fa-regular fa-clock"></i> ${activeDate.toLocaleString("en-US", options)} at ${activeDate.toLocaleTimeString()}`;
+        showDates.appendChild(lastActive);
+         
+        // DUE DATE
+        // card.due, card.dueComplete
+        if(card.due !== null) {
+        const dueDate = new Date(card.due);
+        const showDueDate = document.createElement('span');
+        showDueDate.classList.add('due-date');
+        showDueDate.innerHTML += `<i title="Date Due" class="fa-regular fa-bell"></i> ${dueDate.toLocaleString("en-US", options)} at ${dueDate.toLocaleTimeString()}`;
+        if(card.dueComplete) {
+          showDueDate.innerHTML += `<span class="due-complete"> complete <i class="fa-solid fa-check"></i></span>`
+        } else {
+          const today = new Date();
+          if (dueDate[Symbol.toPrimitive]('number') < today[Symbol.toPrimitive]('number')) {
+            showDueDate.innerHTML += `<span class="due-overdue"> overdue <i class="fa-solid fa-triangle-exclamation"></i></span>`
+          }
+        }
+        showDates.appendChild(showDueDate);
+        }
+        cardSection.appendChild(showDates);
+        
+        
+        // MEMBERS
+        // card.members []
+        if(card.members.length) {
+          const membersList = document.createElement('ul');
+          membersList.classList.add('members-list');
+          const membersTitle = document.createElement('li');
+          membersTitle.innerHTML = `<b>Members (${card.members.length}):</b>`;
+          membersList.appendChild(membersTitle);
+          card.members.forEach(member => {
+            const memberItem = document.createElement('li');
+            memberItem.innerHTML = `<img class="member-avatar" src=${member.avatar} /> <span class="member-name">${member.fullName}</span>`;
+            membersList.appendChild(memberItem);
+          })
+          cardSection.appendChild(membersList);
+        }
+        
+        // CARD DESC
+        // convert markdown to HTML
         const cardDesc = await unified().use(remarkParse)
           .use(remarkGfm)
           .use(remarkRehype)
           .use(rehypeStringify).process(card.desc);
-        // DISPLAY THE DESCRIPTION SECTION ONLY IF THERE IS A CARD DESCRIPTION
+        // display the description div only if the card has a description
         if (card.desc !== '') {
           cardSection.innerHTML += `
             <section class="card-desc">${cardDesc}</section>
         `
-        }   
+        }
+        
+        // ATTACHMENTS 
+        // card.attachments []
+        if(card.attachments.length) {
+          const attachmentsDiv = document.createElement('div');
+          attachmentsDiv.classList.add('attachments');
+          attachmentsDiv.innerHTML += `<h4>Attachments (${card.attachments.length}):</h4>`;
+          const attachmentsList = document.createElement('ul');
+          card.attachments.forEach(attachment => {
+            const attachmentLi = document.createElement('li');
+            attachmentLi.innerHTML = `${attachment.name}: <a href=${attachment.url}>${attachment.url}</a>`;
+            attachmentsList.appendChild(attachmentLi);
+          })
+          attachmentsDiv.appendChild(attachmentsList);
+          cardSection.appendChild(attachmentsDiv);
+        }
         listSection.appendChild(cardSection);
       })
     })
