d4862c63617ef74d595aa2fdf20b6f94c9d73e8a
Danielle Lyle <karendlyle@gmail.com>
2022-07-22 15:51:46 +0200

    csp

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; font-src '*'; img-src '*'; script-src 'self' 'https://*.trellocdn.com'; style-src 'self' 'https://*.trello.com'; frame-src 'self'"
+    "default-src 'self'; font-src '*'; img-src '*'; script-src 'self', 'https://*.trellocdn.com'; style-src 'self', 'https://*.trello.com'; frame-src 'self'"
   );
   next();
 })
