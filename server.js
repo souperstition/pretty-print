0235e665c26ff9990cec6e7065ceac19fe573d8e
Danielle Lyle <karendlyle@gmail.com>
2022-07-22 15:08:46 +0200

    csp

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(
       scriptSrc: ["'self'", "https://*.trello.com"],
       objectSrc: ["'none'"],
       upgradeInsecureRequests: [],
-      frameAncestors: ["'self'", "https://*.trello.com"],
+      frameAncestors: ["'self'", "'https://*.trello.com'"],
     },
   })
 );
