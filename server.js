f4398e7f6310eda4c4c2eeee5d19ef91290287d4
Danielle Lyle <karendlyle@gmail.com>
2022-07-22 16:43:35 +0200

    csp

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
 const compression = require('compression');
 const cors = require('cors');
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(compression());
 app.use(cors({ origin: '*' }));
-app.use((req, res, next) => {
-  res.setHeader(
-    "Permissions-Policy",
-    'geolocation=(self "https://trello.com/"), microphone=()'
-  );
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self'; font-src *; img-src *; script-src 'self' https://*.trellocdn.com https://*.trello.com https://cdn.skypack.dev https://esm.sh https://*.fontawesome.com; style-src 'self' 'unsafe-inline' https://*.trello.com https://fonts.googleapis.com https://*.fontawesome.com; frame-src 'self'; connect-src 'self' https://*.fontawesome.com"
-  );
-  res.setHeader(
-    'Strict-Transport-Security',
-    "max-age=63072000; preload=true; includeSubDomains"
-  );
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    '*'
-  );
-  res.setHeader(
-    'Orgin-Agent-Cluster',
-    '?false'
-  );
-  next();
-});
+
+app.use(helmet.contentSecurityPolicy({
+  useDefaults: false,
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "https://*.trellocdn.com", "https://*.trello.com", "https://cdn.skypack.dev https://esm.sh", "https://*.fontawesome.com"],
+    imgSrc: ["*"],
+    fontSrc: ["*"],
+    styleSrc: ["'self", "'unsafe-inline'", "https://*.trello.com", "https://fonts.googleapis.com", "https://*.fontawesome.com"],
+    frameSrc: ["'self'"],
+    connectSrc: ["'self'", "https://*.fontawesome.com"],
+    objectSrc: ["'none'"],
+    upgradeInsecureRequests: [],
+  },
+}));
+
+
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Permissions-Policy",
+//     'geolocation=(self "https://trello.com/"), microphone=()'
+//   );
+//   res.setHeader(
+//     'Content-Security-Policy',
+//     "default-src 'self'; font-src *; img-src *; script-src 'self' https://*.trellocdn.com https://*.trello.com https://cdn.skypack.dev https://esm.sh https://*.fontawesome.com; style-src 'self' 'unsafe-inline' https://*.trello.com https://fonts.googleapis.com https://*.fontawesome.com; frame-src 'self'; connect-src 'self' https://*.fontawesome.com"
+//   );
+//   res.setHeader(
+//     'Strict-Transport-Security',
+//     "max-age=63072000; preload=true; includeSubDomains"
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Origin',
+//     '*'
+//   );
+//   res.setHeader(
+//     'Orgin-Agent-Cluster',
+//     '?false'
+//   );
+//   next();
+// });
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
