6c650cdc01a6e61e4cbe2270b48e1bc502f531c7
Danielle Lyle <karendlyle@gmail.com>
2022-07-22 13:58:24 +0200

    security

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({ origin: 'https://trello.com' }));
 app.use((req, res, next) => {
   res.setHeader(
     "Permissions-Policy",
-    'geolocation=(self "https://example.com"), microphone=()'
+    'geolocation=(self "https://pretty-print-board.herokuapp.com/"), microphone=()'
   );
   next();
 });
