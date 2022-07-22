0bad8c9d7d1cacf0cc81463a698b67f9ebb2fdc3
Danielle Lyle <karendlyle@gmail.com>
2022-07-22 14:43:09 +0200

    content security policy

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ app.use(cors({ origin: 'https://trello.com' }));
 //   );
 //   next();
 // });
-// app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "https://trello.com"],
+      "style-src": null,
+    },
+  })
+);
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
